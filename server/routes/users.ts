4abb74b9796fc3e1b690557bf992b65f58628c49
Roben-hou <coppinkausar@gmail.com>
2026-06-12 20:14:10 +0800

    claude test

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,13 +1,35 @@
 import Router from 'koa-router';
+import { Context, Next } from 'koa';
 
 const router = new Router();
 
 router.prefix('/users');
 
-router.get('/', function (ctx, next) {
+router.get('/', async (ctx, next: Next) => {
   ctx.body = 'this is a users response!';
 });
-
+router.get('/info', async (ctx, next: Next) => {
+  const id = ctx.query.id
+  ctx.body = {
+    id: id,
+    name: '致秋'
+  }
+})
+router.get('/:id', async (ctx, next: Next) => {
+  const id = ctx.params.id
+  ctx.body = {
+    id: id,
+    name: '致秋'
+  }
+});
+router.post('/create', async (ctx, next: Next) => {
+  const { id = '', name = '' } = ctx.request.body as { id: number, name: string }
+  ctx.body = {
+    id,
+    name,
+    message: '创建成功'
+  }
+});
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response';
 });
