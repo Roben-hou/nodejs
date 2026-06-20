b6519b5ea8dab2f729b7146c3036f4784ef2e5fc
Roben-hou <coppinkausar@gmail.com>
2026-06-20 23:00:35 +0800

    路由

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,1 +1,22 @@
 import { RouterContext } from 'koa-router'
+import { Next } from 'koa'
+import jwt from 'jsonwebtoken'
+
+export const authMiddleware = async (ctx: RouterContext, next: Next) => {
+    console.log('收到的header:', ctx.headers.authorization)
+    const token = ctx.headers.authorization?.split(' ')[1]
+
+  if (!token) {
+    ctx.status = 401
+    ctx.body = { message: '未登录' }
+    return
+  }
+  try {
+    const decoded = jwt.verify(token, 'secret')
+    ctx.state.user = decoded
+    await next()
+  } catch (err) {
+    ctx.status = 401
+    ctx.body = { message: 'token无效或已过期' }
+  }
+}
--- server/routes/records.ts
+++ server/routes/records.ts
@@ -1,11 +1,14 @@
 import Router from 'koa-router';
 import { RecordController } from '../controllers/records'
+import { authMiddleware } from '../middlewares/auth'
+
+
 
 const router = new Router();
 
 router.prefix('/records');
 
-router.get('/', RecordController.getList);
+router.get('/', authMiddleware, RecordController.getList)
 router.post('/create', RecordController.create)
 router.get('/stats', RecordController.getStats)
 router.delete('/:id', RecordController.delete)
