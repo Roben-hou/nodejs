686ef5b243b88bb78ed9c27bdd1868be8374cc09
Roben-hou <coppinkausar@gmail.com>
2026-07-19 22:39:32 +0800

    changerouter

--- server/routes/family.ts
+++ server/routes/family.ts
@@ -8,7 +8,7 @@ router.get('/my', authMiddleware, FamilyController.getMyFamilies);
 router.post('/create', authMiddleware, FamilyController.create);
 router.get('/invitations', authMiddleware, FamilyController.getMyInvitations);
 router.post('/apply', authMiddleware, FamilyController.apply);
-router.post('/:id/invite', authMiddleware, FamilyController.invite);
+router.post('/invite/send/:id', authMiddleware, FamilyController.invite);
 router.put('/accept/:id', authMiddleware, FamilyController.requestJoin);
 router.put('/acceptInvite/:id', authMiddleware, FamilyController.acceptInvite);
 router.put('/reject/:id', authMiddleware, FamilyController.rejectRequest);
