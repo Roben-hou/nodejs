fe8cb4a0e565c0ff6e67fc6849f274050bf2db41
Roben-hou <coppinkausar@gmail.com>
2026-07-20 14:05:07 +0800

    authmiddleware

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,20 +1,23 @@
 import { RouterContext } from 'koa-router'
 import { Next } from 'koa'
 import jwt from 'jsonwebtoken'
-
 export const authMiddleware = async (ctx: RouterContext, next: Next) => {
-    const token = ctx.headers.authorization?.split(' ')[1]
+  const token = ctx.headers.authorization?.split(' ')[1]
+
   if (!token) {
     ctx.status = 401
     ctx.body = { message: '未登录' }
     return
   }
+
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!)
-    ctx.state.user = decoded
-    await next()
+    ctx.state.user = jwt.verify(token, process.env.JWT_SECRET!)
   } catch (err) {
     ctx.status = 401
     ctx.body = { message: 'token无效或已过期' }
+    return
   }
+
+  // 必须放到 try/catch 外面
+  await next()
 }
